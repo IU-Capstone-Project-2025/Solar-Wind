4907aef08128afeee4cd143a501453ced7613c1f
i.lobazov <i.lobazov@innopolis.university>
2025-06-11 01:08:15 +0300

    Create boilerplate code for backend, dockerize backend, and remove pg data

---
+++ backend/deckShuffle/src/main/java/deckShuffle/app/DeckShuffleApplication.java
@@ -0,0 +1,13 @@
+package deckShuffle.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeckShuffleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeckShuffleApplication.class, args);
+	}
+
+}
---
+++ backend/likes/src/main/java/likes/app/controllers/AliveController.java
@@ -0,0 +1,17 @@
+package likes.app.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class AliveController {
+    @GetMapping("/")
+    public String hello() {
+        return "hi from likes.app";
+    }
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
