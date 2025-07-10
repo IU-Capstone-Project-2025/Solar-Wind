f6dffa6ef4701640ad7c01bafd496dfea8c0a8e1
XriXis <xzsaythjpf@gmail.com>
2025-07-10 17:28:28 +0300

    Add swagger extension for authorization headers display
    (cherry picked from commit f29926e4bfc664c3e226efea83d0c19804e57a93)

---
+++ backend/common-modules/security-module/src/main/java/com/solarwind/securityModule/components/SwaggerSecurityExtension.java
@@ -0,0 +1,54 @@
+package com.solarwind.securityModule.components;
+
+import com.solarwind.securityModule.annotation.Secured;
+import com.solarwind.securityModule.service.DatabaseSourceReader;
+import com.solarwind.securityModule.service.PropertyTokenSourceReader;
+import com.solarwind.securityModule.service.TokenSourceReader;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.media.StringSchema;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springdoc.core.customizers.OperationCustomizer;
+
+
+@Component
+public class SwaggerSecurityExtension implements OperationCustomizer {
+
+    @Override
+    public Operation customize(Operation operation, HandlerMethod handlerMethod) {
+        Secured secured = handlerMethod.getMethodAnnotation(Secured.class);
+        if (secured == null) {
+            secured = handlerMethod.getBeanType().getAnnotation(Secured.class);
+        }
+        if (secured == null) {
+            return operation;
+        }
+        Class<? extends TokenSourceReader> tokenSource = secured.value();
+
+        if (tokenSource.equals(DatabaseSourceReader.class)) {
+            addHeader(operation, "Authorization", "Access token");
+            addHeader(operation, "Authorization-telegram-id", "Telegram User ID");
+        } else if (tokenSource.equals(PropertyTokenSourceReader.class)) {
+            addHeader(operation, "Authorize", "Simple token");
+        }
+
+        return operation;
+    }
+
+    private void addHeader(Operation operation, String name, String description) {
+        if (operation.getParameters() != null && operation.getParameters().stream()
+                .anyMatch(p -> name.equalsIgnoreCase(p.getName()))) {
+            return;
+        }
+
+        Parameter header = new Parameter()
+                .in("header")
+                .required(true)
+                .name(name)
+                .description(description)
+                .schema(new StringSchema());
+
+        operation.addParametersItem(header);
+    }
+}
