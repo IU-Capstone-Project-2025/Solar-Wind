b481821c2e8bc215537c02168fa0be795c5b41b0
XriXis <xzsaythjpf@gmail.com>
2025-07-20 19:33:18 +0300

    Add telegram auto-notifications

--- backend/notifications/src/main/java/notifications/app/controllers/DispatcherController.java
+++ backend/notifications/src/main/java/notifications/app/controllers/DispatcherController.java
@@ -4,6 +4,8 @@ import com.solarwind.securityModule.annotation.Secured;
 import com.solarwind.securityModule.service.DatabaseSourceReader;
 import com.solarwind.securityModule.service.PropertyTokenSourceReader;
 import notifications.app.dto.NotificationDto;
+import notifications.app.services.TelegramService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -20,6 +22,9 @@ import java.util.HashMap;
 public class DispatcherController {
     private final static HashMap<Long, ArrayList<NotificationDto>> allNotifications = new HashMap<>();
 
+    @Autowired
+    private TelegramService telegramService;
+
     @Secured(DatabaseSourceReader.class)
     @GetMapping("/")
     public ArrayList<NotificationDto> dispatcher(@RequestHeader("Authorization-telegram-id") Long owner) {
@@ -31,11 +36,15 @@ public class DispatcherController {
     @Secured(PropertyTokenSourceReader.class)
     @PostMapping("/")
     public void publish(@RequestParam Long user1, @RequestParam Long user2) {
-        for (var user : new Long[]{user1, user2}) {
+        for (var userPair : new Long[][]{{user1, user2}, {user2, user1}}) {
+            var user = userPair[0];
             if (!allNotifications.containsKey(user)) {
                 allNotifications.put(user, new ArrayList<>());
             }
-            allNotifications.get(user).add(new NotificationDto("You have the match!", user1, user2));
+            NotificationDto notification = new NotificationDto("You have the match! ", user1, user2);
+            allNotifications.get(user).add(notification);
+            telegramService.sendTelegramMessage(user,
+                    notification.Text() + "https://web.telegram.org/a/#" + userPair[1]);
         }
     }
 }
